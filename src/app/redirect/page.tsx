a26f52792481a3ef69ddc85fec094eeaa8482674
Issa Gomes <codedbyissa@gmail.com>
2025-07-31 08:25:36 -0300

    update 3

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -3,15 +3,17 @@
 import { useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 export default function RedirectPage() {
-    const router = useRouter();
-    const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-    useEffect(() => {
-        const to = searchParams.get('to') || '/';
-        window.history.pushState(null, '', '/');
-        router.replace(to);
-    }, [router, searchParams]);
+  useEffect(() => {
+    const to = searchParams.get('to') || '/';
+    window.history.pushState(null, '', '/');
+    router.replace(to);
+  }, [router, searchParams]);
 
-    return null;
+  return null;
 }
